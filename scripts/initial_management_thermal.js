3599f2e8b1fc6ca24230089b2d4c0e2ab20e13f2
concussed8 <mitchlapierre@gmail.com>
2025-01-19 12:00:58 -0500

    Update initial_management_thermal.js

--- scripts/initial_management_thermal.js
+++ scripts/initial_management_thermal.js
@@ -38,8 +38,9 @@ function validateMandatoryFields() {
     if (!checkbox1.checked || !checkbox2.checked || !checkbox3.checked || !checkbox4.checked) {
         alert('Please ensure all mandatory fields are completed.');
     } else {
-        // Redirect to the thermal TBSA and resus page with weight parameter
-window.location.href = `https://concussed8.github.io/Burn-Management-Project/page/thermal_tbsa_and_resus.html?weight=${encodeURIComponent(weightInput)}`;
+      // Redirect to the thermal TBSA and resus page with weight parameter
+window.location.href = `https://concussed8.github.io/Burn-Management-Project/page/thermal_tbsa_and_resus.html?weight=${encodeURIComponent(weightInput)}&ts=${Date.now()}`;
+
 
     }
 }
