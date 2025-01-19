726bbe406fe0b8f1c971e1634d43f1bc96890974
concussed8 <mitchlapierre@gmail.com>
2025-01-19 08:55:53 -0500

    Update page2thermal.js

--- scripts/page2thermal.js
+++ scripts/page2thermal.js
@@ -7,7 +7,7 @@ function validateMandatoryFields() {
         alert('Please ensure the mandatory field is completed.');
     } else if (checkboxYes.checked) {
         // Redirect to the Initial_Management_Checklist for "YES"
-        window.location.href = "./initial_management_thermal.html"; // Relative path for hosting
+        window.location.href = "https://concussed8.github.io/Burn-Management-Project/page/initial_management_thermal.html"; // Full path for GitHub Pages
     } else if (checkboxNo.checked) {
         // Perform an action for "NO" (if applicable)
         alert('Form submitted successfully for NO.');
